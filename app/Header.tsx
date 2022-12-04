a9bc1d4ae703e1455f3d2215b1518cb7f9455ecf
oliveryao09 <695353020@qq.com>
2022-12-04 14:12:34 +1300

    created login & logout header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,11 +1,35 @@
 import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+import LogoutButton from './LogoutButton';
 
 function Header() {
+  const session = true;
+
+  // Appears when the user signs in
+  if (session)
+    return (
+      <header className="sticky top-0 z-50 bg-white flex justify-between items-center p-10 shadow-sm">
+        <div className="flex space-x-2">
+          <Image
+            className="rounded-full mx-2 object-contain"
+            src="https://links.papareact.com/jne"
+            height={10}
+            width={50}
+            alt="Logo"
+          />
+          <div>
+            <p className="text-blue-400">Logged in as:</p>
+            <p className="font-bold text-lg">Oliver Yao</p>
+          </div>
+        </div>
+        <LogoutButton />
+      </header>
+    );
+
   return (
-    <header>
-      <div>
-        <div>
+    <header className="sticky top-0 z-50 bg-white flex justify-center items-center p-10 shadow-sm">
+      <div className="flex flex-col items-center space-y-5">
+        <div className="flex space-x-2 items-center">
           <Image
             src="https://links.papareact.com/jne"
             height={10}
@@ -13,8 +37,14 @@ function Header() {
             alt="Logo"
           />
 
-          <p className="text-">Welcome to Meta Messeenger</p>
+          <p className="text-blue-400">Welcome to Meta Messeenger</p>
         </div>
+        <Link
+          href="/auth/signin"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Sign In
+        </Link>
       </div>
     </header>
   );
