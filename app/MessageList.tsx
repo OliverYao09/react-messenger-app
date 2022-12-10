314e40105efe00736c850a41b0d3230bac19d049
oliveryao09 <695353020@qq.com>
2022-12-10 15:23:09 +1300

    Display conversation messages and avatar
    Added a component to display user's avatar and conversation messages

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
+'use client';
+
+import useSWR from 'swr';
+import fetcher from '../utils/fetchMessages';
+import MessageComponent from './MessageComponent';
 
 function MessageList() {
+  const { data: messages, error, mutate } = useSWR('/api/getMessages', fetcher);
   return (
-    <div>
-      <p>message 1</p>
-      <p>message 2</p>
-      <p>message 3</p>
-      <p>message 3</p>
-      <p>message 3</p>
-      <p>message 3</p>
-      <p>message 3</p>
-      <p>message 3</p>
-      <p>message 3</p>
+    <div className="space-y-5 px-5 pt-8 pb-32 max-w-2xl xl:max-w-4xl mx-auto">
+      {messages?.map((message) => (
+        <MessageComponent key={message.id} message={message} />
+      ))}
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [
-      'links.papareact.com',
-      'scontent.fwlg3-1.fna.fbcdn.net/v/t31.18172-8/22548900_507142199652776_5596273874271410386_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8lNbkruW8v4AX_adK79&_nc_ht=scontent.fwlg3-1.fna&oh=00_AfAFUF7b_vMMRYGK2NRsO6vdDt38M7TAPM-pDYXc2ZEUbw&oe=63B35A5E',
-    ],
+    domains: ['links.papareact.com', 'scontent.fwlg3-1.fna.fbcdn.net'],
   },
   experimental: {
     appDir: true,
