5fa3355c23bd5bb3acec6ee78e2d26ba493c3cfc
oliveryao09 <695353020@qq.com>
2022-12-11 14:55:39 +1300

    Added server side rendering
    the data will be loading beforehand on the server, so which means we will get all of the messages when we first load the app without sending a request
    the data will be loading beforehand on the server, so which means we will get all of the messages when we first load the app without sending a request

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import MessageList from './MessageList';
 import ChatInput from './ChatInput';
 
-function HomePage() {
+async function HomePage() {
+  // Server side rendering -loading the data beforehand on the server
+  const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
+    (res) => res.json()
+  );
+
+  console.log(data);
+
   return (
     <main>
       <MessageList />
