91e165417969a8aeba1d6cd80cccfaefe0f500c5
oliveryao09 <695353020@qq.com>
2022-12-11 15:32:11 +1300

    hide the secret key

--- pusher.ts
+++ pusher.ts
@@ -4,7 +4,7 @@ import ClientPusher from 'pusher-js';
 export const serverPusher = new Pusher({
   appId: '1523085',
   key: 'b9f506ba5bc8b5b96266',
-  secret: '61b2ce15ef46940b53bd',
+  secret: `${process.env.SECRET_KEY}`,
   cluster: 'ap1',
   useTLS: true,
 });
