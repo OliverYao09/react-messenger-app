7490a76edfee3cbec0a9c9234a5888a39bd0f5ce
oliveryao09 <695353020@qq.com>
2022-12-18 13:51:06 +1300

    add loading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import MessageList from './MessageList';
 import ChatInput from './ChatInput';
+import { Message } from '../typings';
 
 async function HomePage() {
   // Server side rendering -loading the data beforehand on the server
-  const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
-    (res) => res.json()
-  );
+  const data = await fetch(
+    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api/getMessages`
+  ).then((res) => res.json());
 
-  console.log(data);
+  const messages: Message[] = data.messages;
 
   return (
     <main>
-      <MessageList />
+      <MessageList initialMessagees={messages} />
       <ChatInput />
     </main>
   );
